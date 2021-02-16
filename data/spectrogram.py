cabf25aaba92679d98b5db548dc4f5c250e534c3
hslau44 <hslau44@gmail.com>
2021-02-16 23:18:24 +0000

    add csipwr pair

--- data/spectrogram.py
+++ data/spectrogram.py
@@ -7,11 +7,11 @@ from os.path import isfile, join
 from sklearn.preprocessing import MinMaxScaler
 
 
-columns = [f"col_{i+1}" for i in range(501)]
 
-def csv_to_arr(path,columns=columns):
+
+def csv_to_arr(path):
     """Import single csv file as numpy.ndarray"""
-    arr = pd.read_csv(path, names=columns).to_numpy()
+    arr = pd.read_csv(path, header=None).to_numpy()
     return arr
 
 def sliding(arr,window_size=None,slide_size=None):
@@ -42,32 +42,33 @@ def normalize_arr(arr,axis=0):
     return arr
 
 
-def import_spectrograms(filepaths,**kwarg):
+def import_spectrograms(filepaths):
     """
     import all spectrogram in the filepaths as one array
     """
     mats = []
     for p in filepaths:
-        arr = csv_to_arr(p) # ,kwarg['columns']
-        arr = normalize_arr(arr,axis=0)
-        # arr = sliding(arr,kwarg['window_size'],kwarg['slide_size'])
+        arr = csv_to_arr(p)
+        # arr = normalize_arr(arr,axis=0)
         arr = arr.reshape(-1,*arr.shape)
         mats.append(arr)
     return np.concatenate(mats,axis=0)
 
 
-def import_data(directory, columns=columns, **kwarg):
+def import_data(directory):
     """
     import all spectrogram in the directory
     """
-    print("Import Data")
+    print("Importing Data ",end='')
     data = {'X':[],'y':[]}
     for label in os.listdir(directory):
+        print('>',end='')
         files = [directory+'/'+label+'/'+i for i in os.listdir(directory+'/'+label)]
-        X = import_spectrograms(files,**kwarg)
+        X = import_spectrograms(files)
         y = np.full(X.shape[0], label)
         data['X'].append(X)
         data['y'].append(y)
+    print(" Complete")
     return np.concatenate(data['X']), np.concatenate(data['y'])
 
 
@@ -85,6 +86,32 @@ def import_pair_data(directory):
         available_pairs = np.intersect1d(pfiles_nuc1,pfiles_nuc2).tolist()
         files_nuc1 = [directory+'/'+label+'/'+'nuc1'+'/'+'nuc1_'+pfilename+'.csv' for pfilename in available_pairs]
         files_nuc2 = [directory+'/'+label+'/'+'nuc2'+'/'+'nuc2_'+pfilename+'.csv' for pfilename in available_pairs]
+        # importing
+        X1 = import_spectrograms(files_nuc1)
+        X2 = import_spectrograms(files_nuc2)
+        y = np.full(X1.shape[0], label)
+        assert X1.shape[0] == X2.shape[0]
+        data['X1'].append(X1)
+        data['X2'].append(X2)
+        data['y'].append(y)
+    print(" Complete")
+    return np.concatenate(data['X1']), np.concatenate(data['X2']), np.concatenate(data['y'])
+
+
+def import_CsiPwr_data(directory):
+    """
+    import all spectrogram (in pair) in the directory
+    """
+    print("Importing Data ",end='')
+    data = {'X1':[],'X2':[],'y':[]}
+    for label in os.listdir(directory):
+        print('>',end='')
+        # selcting available pairs
+        pfiles_nuc1 = [f.split('.')[0] for f in os.listdir(directory+'/'+label+'/'+'csi')]
+        pfiles_nuc2 = [f.split('.')[0] for f in os.listdir(directory+'/'+label+'/'+'pwr')]
+        available_pairs = np.intersect1d(pfiles_nuc1,pfiles_nuc2).tolist()
+        files_nuc1 = [directory+'/'+label+'/'+'csi'+'/'+pfilename+'.csv' for pfilename in available_pairs]
+        files_nuc2 = [directory+'/'+label+'/'+'pwr'+'/'+pfilename+'.csv' for pfilename in available_pairs]
         # importing
         X1 = import_spectrograms(files_nuc1)
         X2 = import_spectrograms(files_nuc2)
