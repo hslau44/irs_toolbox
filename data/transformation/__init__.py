9059ef6d1a11f23e12f5f8286375f9d93f78e617
hslau44 <hslau44@gmail.com>
2021-02-19 11:19:32 +0000

    update slide, slide_augmentation to be depreciated

--- data/transformation/__init__.py
+++ data/transformation/__init__.py
@@ -27,6 +27,10 @@ def major_vote(arr, impurity_threshold=0.01):
         result = counter.most_common()[0][0]
     return result
 
+def major_vote2(arr):
+    for a in arr:
+        a = major_vote(a)
+
 
 def slide_augmentation(X, y, z, window_size, slide_size, skip_labels=None):
     """ Data augmentation, create sample by sliding
@@ -62,6 +66,36 @@ def slide_augmentation(X, y, z, window_size, slide_size, skip_labels=None):
             data['y'].append(label)
 
     return np.array(data['X']),np.array(data['y']), z
+
+
+def slide(*arrays, window_size, slide_size):
+    """
+    slide the 0 axis of the arrays to create new data with window_size
+    """
+    length = arrays[0].shape[0]
+
+    for arr in arrays:
+        assert arr.shape[0] == length, "all array in arrays must has same length"
+
+    assert window_size <= length, "window size must not be larger than the length of arrays"
+
+    new_arrays = []
+
+    for arr in arrays:
+
+        data = []
+
+        for i in range(0, length-window_size+1, slide_size):
+
+            d = arr[i:i+window_size]
+
+            data.append(d)
+
+        data = np.array(data)
+
+        new_arrays.append(data)
+
+    return new_arrays
 
 
 def stacking(x):
@@ -106,121 +140,19 @@ def resampling(X,y,z,oversampling=True):
     X,y,z = shuffle(X[idx_ls],y[idx_ls],z[idx_ls])
     return X,y,z
 
+def selections(*arg,**kwarg):
+    size = int(arg[0].shape[0]*kwarg['p'])
+    index = np.arange(0,arg[0].shape[0])
+    test_selection = np.random.choice(index,size,replace=False)
+    train_selection = np.array([i for i in index if i not in test_selection])
+    return [i[train_selection] for i in arg],[i[test_selection] for i in arg]
+
+
 def label_encode(label):
     enc = LabelEncoder()
     label = enc.fit_transform(label)
     return label, enc
 
-
-# class DatasetObject:
-#
-#     def __init__(self):
-#
-#         self.data = []
-#
-#         self.encoders = np.empty(shape=(1,3),dtype=np.ndarray)
-#
-#     def import_data(self, features_ls, labels_ls):
-#         """
-#         Store data based on the division in the list
-#
-#         features_ls(list(numpy.ndarray)): the features seperated by list
-#         labels_ls(list(numpy.ndarray)): the labels seperated by list
-#         """
-#         assert len(features_ls) == len(labels_ls)
-#         # Create emepty numpy array for storing data
-#         self.data = np.empty(shape=(len(features_ls),3),dtype=np.ndarray)
-#
-#         for item_idx,(X,y) in enumerate(zip(features_ls,labels_ls)):
-#
-#             z = np.full_like(y,item_idx)
-#
-#             assert X.shape[0] == y.shape[0] == z.shape[0]
-#
-#             self.data[item_idx,0] = X
-#             self.data[item_idx,1] = y
-#             self.data[item_idx,2] = z
-#
-#             # print(f'index {item_idx} arrays sizes ------ X: ',X.shape,' Y: ',y.shape,' Z: ',z.shape)
-#
-#         # print('size of DatasetObject ------ : ',self.data.shape)
-#
-#         return
-#
-#     def __call__(self, idxs=None, return_train_sets=False):
-#         """
-#         Return data base on the index, or return all data if not specified
-#
-#         Input:
-#         idxs(list/bool): the list of index that the data to be queried, if None all data will be returned
-#         return_train_sets(bool): return remain data no in idxs as training set
-#
-#         Return:
-#         data:  Return based on return_train_sets
-#         if return_train_sets == True, data = (tuple(object,object,object), tuple(object,object,object))
-#         else, data = (tuple(object,object,object))
-#         """
-#
-#         if idxs != None:
-#
-#             X  = np.concatenate(self.data[idxs,0],axis=0)
-#             y  = np.concatenate(self.data[idxs,1],axis=0)
-#             z  = np.concatenate(self.data[idxs,2],axis=0)
-#
-#             if return_train_sets == True:
-#
-#                 q = [i for i in range(self.data.shape[0])]
-#                 for idx in idxs:
-#                     q.remove(idx)
-#
-#                 X_train = np.concatenate(self.data[q,0],axis=0)
-#                 y_train = np.concatenate(self.data[q,1],axis=0)
-#                 z_train = np.concatenate(self.data[q,2],axis=0)
-#
-#                 print('train set:',q,'\ttest set:',idxs)
-#                 return (X_train,y_train,z_train),(X,y,z)
-#
-#             else:
-#
-#                 print('dataset:',idxs)
-#
-#                 return (X,y,z)
-#
-#         else:
-#
-#             X  = np.concatenate(self.data[:,0],axis=0)
-#             y  = np.concatenate(self.data[:,1],axis=0)
-#             z  = np.concatenate(self.data[:,2],axis=0)
-#
-#             return (X, y, z)
-#
-#     def data_transform(self,func,axis=0,col=None):
-#         """
-#         Apply data transformation on each index of data
-#
-#         func:
-#         for axis = 0 : X,y,z = func(X,y,z)
-#         for axis = 1 : k = func(k) where k in {X (col=0), y (col=1), z (col=2)}
-#         """
-#         if axis == 0:
-#             for i in range(self.data.shape[0]):
-#                 self.data[i,0],self.data[i,1],self.data[i,2] = func(self.data[i,0],self.data[i,1],self.data[i,2])
-#         elif axis == 1:
-#             for i in range(self.data.shape[0]):
-#                 self.data[i,col] = func(self.data[i,col])
-#         else:
-#             print('No transformation is made')
-#         return
-#
-#     def shape(self):
-#         """
-#         Print the shape
-#         """
-#         for i in range(self.data.shape[0]):
-#             print(f'index {i} arrays sizes ------ X: ',self.data[i,0].shape,' Y: ',
-#                   self.data[i,1].shape,' Z: ',self.data[i,2].shape)
-#         print('size of DatasetObject ------ : ',self.data.shape)
-#         return
 
 def create_dataloaders(X_train, y_train, X_test, y_test, train_batch_sizes=64, test_batch_sizes=200, num_workers=1):
     traindataset = TensorDataset(Tensor(X_train),Tensor(y_train).long())
@@ -228,11 +160,3 @@ def create_dataloaders(X_train, y_train, X_test, y_test, train_batch_sizes=64, t
     train_loader = DataLoader(traindataset, batch_size=train_batch_sizes, shuffle=True, num_workers=num_workers, drop_last=True)
     test_loader = DataLoader(testdataset, batch_size=test_batch_sizes, shuffle=True, num_workers=num_workers)
     return train_loader, test_loader
-
-
-def selections(*arg,**kwarg):
-    size = int(arg[0].shape[0]*kwarg['p'])
-    index = np.arange(0,arg[0].shape[0])
-    test_selection = np.random.choice(index,size,replace=False)
-    train_selection = np.array([i for i in index if i not in test_selection])
-    return [i[train_selection] for i in arg],[i[test_selection] for i in arg]
