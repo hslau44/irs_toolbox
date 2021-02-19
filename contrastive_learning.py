bede0d9b3c144624bc92ef69ed0a99c693ab570b
hslau44 <hslau44@gmail.com>
2021-02-19 16:23:17 +0000

    changing for data import

--- contrastive_learning.py
+++ contrastive_learning.py
@@ -3,22 +3,23 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 import torch
 import torch.nn as nn
-from torch import Tensor
-from torch.utils.data import DataLoader, TensorDataset
 from torch.nn import functional as F
-from torchsummary import summary
-import torchvision
 
-from data.spectrogram import import_data,import_pair_data,import_CsiPwr_data
-from data.process_data import label_encode, create_dataloaders, resampling,selections
-
-from models import ED_module, Classifier
-from losses import SupConLoss, NT_Xent
-from train import train as finetuning
-from train import evaluation,make_directory,save_checkpoint
-from models.self_supervised import Projection_head
-from models.baseline import Encoder_F as Encoder
-from models.cnn import create_vgg16
+# from torch import Tensor
+# from torch.utils.data import DataLoader, TensorDataset
+# from torch.nn import functional as F
+# from torchsummary import summary
+# import torchvision
+#
+#
+#
+# from models import ED_module, Classifier
+# from losses import SupConLoss, NT_Xent
+# from train import train as finetuning
+# from train import evaluation,make_directory,save_checkpoint
+# from models.self_supervised import Projection_head
+# from models.baseline import Encoder_F as Encoder
+# from models.cnn import create_vgg16
 
 
 # random seed
@@ -32,83 +33,99 @@ parallel = True
 num_workers = 0
 
 # data setting
-loc_dirc = 'E:/external_data/Experiment4/Spectrogram_data_csv_files/CSI_PWR_pair'
+loc_dirc = 'E:/external_data/Experiment4/Spectrogram_data_csv_files/CSI_data'
 remote_dirc = './data/csi_pwr'
 PATH = '.' # './'
 EXP_NAME = 'Encoder_vgg16_mode_normal_on_exp4_s_resample_regularize'
 
 
-dirc = remote_dirc
-mode = 2
+DIRC = loc_dirc
+mode = 1
 p = None
 resample = True
-pre_train_epochs = 1600
-fine_tune_epochs = 300
+pre_train_epochs = 1
+fine_tune_epochs = 1
 bsz = 64
 parallel = True
 csi_out_size = (2,3)
 pwr_out_size = (3,1)
 
+from data import create_dataloaders
+from data.spectrogram import import_data, import_pair_data, import_CsiPwr_data
+from data.transformation import label_encode,resampling
+from sklearn.model_selection import train_test_split
+from torch import Tensor
+from torch.utils.data import DataLoader, TensorDataset
 
-def prepare_data(dirc):
-    X,y  = import_data(dirc)
-    X = X.reshape(*X.shape,1).transpose(0,3,1,2)
-    y,lb = label_encode(y)
-    return X,y,lb
-
-def prepare_dataloader(dirc,resample=None,train_batch_sizes=bsz):
-    X,y,lb = prepare_data(dirc)
-    X_train, X_test, y_train, y_test = train_test_split(X,y, test_size=0.2, random_state=42)
-    if resample:
-        X_train,y_train,_ = resampling(X_train,y_train,y_train,oversampling=True)
-        X_test, y_test,_ = resampling(X_test, y_test,y_test,oversampling=False)
-    train_loader, test_loader = create_dataloaders(X_train, y_train, X_test, y_test, train_batch_sizes=train_batch_sizes, test_batch_sizes=2000, num_workers=num_workers)
-    return train_loader, test_loader,lb
-
-def reshape_axis1(X):
-    """
-    To be depreciated
-    """
-    assert len(X.shape) > 1, 'must be two dimensional'
-    return X.reshape(X.shape[0]*X.shape[1],1,*X.shape[2:])
-
-def prepare_dataloader_pairdata(dirc,mode,p=None,resample=None):
-    """
-    Import pair data
-    """
-    if mode == 1:
-        X1,X2,y = import_pair_data(dirc)
-    elif mode == 2:
-        X1,X2,y = import_CsiPwr_data(dirc)
+def prepare_data(mode,directory=DIRC):
+    # pair data
+    if mode == 1 or mode == 2:
+        if mode == 1:
+            X1,X2,y = import_pair_data(directory)
+        elif mode == 2:
+            X1,X2,y = import_CsiPwr_data(directory)
+        # processing
+        y,lb = label_encode(y)
+        X1 = X1.reshape(*X1.shape,1).transpose(0,3,1,2)
+        X2 = X2.reshape(*X2.shape,1).transpose(0,3,1,2)
+        return X1,X2,y,lb
+    # single data
+    elif mode == 0:
+        X,y = import_data(dirc)
+        X = X.reshape(*X.shape,1).transpose(0,3,1,2)
+        y,lb = label_encode(y)
+        return X,y,lb
     else:
-        raise ValueError('Must be 1 or 2 for pair')
+        raise ValueError('Must be 1 or 2 for pair, 0 elsewise')
+    return
 
-    X1 = X1.reshape(*X1.shape,1).transpose(0,3,1,2)
-    X2 = X2.reshape(*X2.shape,1).transpose(0,3,1,2)
-    y,lb = label_encode(y)
 
-    training,validation = selections(X1,X2,y,p=0.2)
-    X_test, y_test = validation[0],validation[2]
+def prepare_dataloader_pairdata(mode,directory=DIRC,batch_size=bsz,joint=None,p=None,resample=None):
 
-    if p:
-        (X_train,y_train) , _ = selections(training[0],training[2],p=p)
-    else:
-        X_train,y_train = training[0],training[2]
+    if mode == 2:
+        joint = None
 
-    if resample:
-        X_train,y_train,_ = resampling(X_train,y_train,y_train,oversampling=True)
-        # X_test, y_test,_ = resampling(X_test, y_test,y_test,oversampling=False)
+    if mode == 0:
+        X,y,lb = prepare_data(mode,directory)
+        X_train, X_test, y_train, y_test = train_test_split(X,y,train_size=0.8)
+        if resample:
+            X_train, y_train = resampling(X_train, y_train, labels=y_train,oversampling=True)
+            X_test, y_test = resampling(X_test, y_test, labels=y_test,oversampling=False)
+        train_loader, test_loader = create_dataloaders(X_train, y_train, X_test, y_test, train_batch_sizes=64, test_batch_sizes=200)
+        return train_loader, test_loader,lb
 
-    ### Dataloader
-    print('X1: ',training[0].shape,' X2: ',training[1].shape)
-    print('X_train: ',X_train.shape,' y_train: ',y_train.shape,' X_test: ',X_test.shape,' y_test: ',y_test.shape)
-    pretraindataset = TensorDataset(Tensor(training[0]),Tensor(training[1]))
-    finetunedataset = TensorDataset(Tensor(X_train),Tensor(y_train).long())
-    validatndataset = TensorDataset(Tensor(X_test), Tensor(y_test).long())
-    pretrain_loader = DataLoader(pretraindataset, batch_size=bsz, shuffle=True, num_workers=num_workers, drop_last=True)
-    finetune_loader = DataLoader(finetunedataset, batch_size=bsz, shuffle=True, num_workers=num_workers, drop_last=True)
-    validatn_loader = DataLoader(validatndataset, batch_size=2000, shuffle=True, num_workers=num_workers)
-    return pretrain_loader, finetune_loader, validatn_loader, lb
+    elif mode == 1 or mode == 2:
+        X1,X2,y,lb = prepare_data(mode,directory)
+        X1_train, X1_test, X2_train, X2_test, y_train, y_test = train_test_split(X1,X2,y,train_size=0.8)
+
+        if joint == True:
+            X_test = np.concatenate(X1_test,X2_test)
+            y_test = np.concatenate(y_test,y_test)
+            X_train = np.concatenate(X1_train,X2_train)
+            y_train = np.concatenate(y_train,y_train)
+        else:
+            X_test = X1_test
+            y_test = y_test
+            X_train = X1_train
+            y_train = y_train
+
+        if p:
+            X_train, _ , y_train, _ = train_test_split(X_train,y_train,train_size=p)
+
+        if resample:
+            X_train, y_train = resampling(X_train, y_train, labels=y_train,oversampling=True)
+            X_test, y_test = resampling(X_test, y_test, labels=y_test,oversampling=False)
+
+        pretraindataset = TensorDataset(Tensor(X1_train),Tensor(X2_train))
+        finetunedataset = TensorDataset(Tensor(X_train),Tensor(y_train).long())
+        validatndataset = TensorDataset(Tensor(X_test), Tensor(y_test).long())
+        pretrain_loader = DataLoader(pretraindataset, batch_size=batch_size, shuffle=True, num_workers=num_workers, drop_last=True)
+        finetune_loader = DataLoader(finetunedataset, batch_size=batch_size, shuffle=True, num_workers=num_workers, drop_last=True)
+        validatn_loader = DataLoader(validatndataset, batch_size=2000, shuffle=True, num_workers=num_workers)
+        print('mode: ',mode,"\tclass: ",lb.classes_)
+        print('X1_train: ',X1_train.shape,'\tX2_train: ',X2_train.shape)
+        print('X_train: ',X_train.shape,'\ty_train: ',y_train.shape,'\tX_test: ',X_test.shape,'\ty_test: ',y_test.shape)
+        return pretrain_loader, finetune_loader, validatn_loader, lb
 
 
 def create_pretrain_model(out_size=(2,3)):
@@ -116,6 +133,11 @@ def create_pretrain_model(out_size=(2,3)):
     enc = create_vgg16(out_size)
     clf = Projection_head(512*out_size[0]*out_size[1],128,head='linear')
     model = ED_module(encoder=enc,decoder=clf)
+    return model
+
+def freeze_network(model):
+    for _, p in model.named_parameters():
+        p.requires_grad = False
     return model
 
 def create_finetune_model(enc=None,out_size=(2,3)):
@@ -128,33 +150,17 @@ def create_finetune_model(enc=None,out_size=(2,3)):
     model = ED_module(encoder=enc,decoder=clf)
     return model
 
-
-def create_criterion():
+def create_criterion(batch_size):
     # External libraries required
-    criterion = NT_Xent(bsz, temperature=0.1, world_size=1)
+    criterion = NT_Xent(batch_size, temperature=0.1, world_size=1)
     return criterion
 
-
-def create_optimizer(mode,model1,model2=None):
-    """mode: {'pretrain','pretrain_2','finetuning'}"""
-    if mode == 'pretrain':
-        optimizer = torch.optim.SGD(list(model1.parameters()), lr=0.0005)
-    elif mode == 'pretrain_2':
-        optimizer = torch.optim.SGD(list(model1.parameters())+list(model2.parameters()), lr=0.0005)
-    elif mode == 'finetuning':
-        optimizer = torch.optim.Adam(list(model1.parameters()), lr=0.0005)
-    else:
-        raise ValueError("mode: {'pretrain','pretrain_2','finetuning'}")
-
+def create_optimizer(mode,model):
+    optimizer = torch.optim.SGD(list(model.parameters()), lr=0.0005)
+    # double # optimizer = torch.optim.SGD(list(model1.parameters())+list(model2.parameters()), lr=0.0005)
     return optimizer
 
-def freeze_network(model):
-    for _, p in model.named_parameters():
-        p.requires_grad = False
-    return model
-
-
-def pretrain(model,train_loader,optimizer,criterion,end,start=1,model2=None,parallel=True):
+def contrastive_pretraining(model,train_loader,optimizer,criterion,end,start=1,model2=None,parallel=True):
 
     # Check device setting
     if parallel == True:
@@ -214,9 +220,8 @@ def pretrain(model,train_loader,optimizer,criterion,end,start=1,model2=None,para
 
     return model,record
 
-
 def record_log(mode,epochs,record,cmtx=None,cls=None):
-    if mode == 'pretrain':
+    if mode == 'pretraining':
         path = make_directory(EXP_NAME+'_pretrained',epoch=epochs,filepath=PATH+'/record/')
         pd.DataFrame(record['train'],columns=['train_loss']).to_csv(path+'_loss.csv')
     elif mode == 'finetuning':
@@ -226,7 +231,6 @@ def record_log(mode,epochs,record,cmtx=None,cls=None):
         cls.to_csv(path+'_report.csv')
         cmtx.to_csv(path+'_cmtx.csv')
     return
-
 
 def save(mode,model,optimizer,epochs):
     if mode == 'pretrain':
@@ -238,42 +242,8 @@ def save(mode,model,optimizer,epochs):
     return
 
 
-def normal():
-    print('normal:  epoch:',300)
-    pretrain_loader, finetune_loader, validatn_loader, lb = prepare_dataloader_pairdata(dirc,mode,p,resample)
-    finetune_model = create_finetune_model(None,csi_out_size)
-    criterion = nn.CrossEntropyLoss()
-    optimizer = torch.optim.Adam(list(finetune_model.parameters()), lr=0.0005)
-    finetune_model, record = finetuning(finetune_model , finetune_loader, criterion, optimizer, 300, 1, validatn_loader, parallel, regularize=True)
-    cmtx,cls = evaluation(finetune_model,validatn_loader,label_encoder=lb)
-    record_log('finetuning',fine_tune_epochs,record,cmtx,cls)
-    return
-
-
 def main():
-    csi_model = create_pretrain_model(csi_out_size)
-    pwr_model = create_pretrain_model(pwr_out_size)
-    criterion = create_criterion()
-    optimizer = create_optimizer('pretrain_2',csi_model,pwr_model)
-    # Data
-    pretrain_loader, finetune_loader, validatn_loader, lb = prepare_dataloader_pairdata(dirc,mode,p,resample)
-    # Pretraining
-    csi_model, record = pretrain(csi_model,pretrain_loader,optimizer,criterion,pre_train_epochs,
-                                 start=1,
-                                 model2=pwr_model,
-                                 parallel=parallel)
-    # save and log
-    record_log('pretrain',pre_train_epochs,record)
-    save('pretrain',csi_model,optimizer,pre_train_epochs)
-    del criterion, optimizer, record, pretrain_loader
-    # Fine-tuning
-    finetune_model = create_finetune_model(csi_model.encoder,csi_out_size)
-    criterion = nn.CrossEntropyLoss()
-    optimizer = create_optimizer('finetuning',finetune_model)
-    finetune_model, record = finetuning(finetune_model , finetune_loader, criterion, optimizer, fine_tune_epochs, 1, validatn_loader, parallel)
-    cmtx,cls = evaluation(finetune_model,validatn_loader,label_encoder=lb)
-    record_log('finetuning',fine_tune_epochs,record,cmtx,cls)
-    save('finetuning',finetune_model,optimizer,fine_tune_epochs)
+    X,y,lb = prepare_data(dirc)
     return
 
 if __name__ == '__main__':
--- data/__init__.py
+++ data/__init__.py
@@ -3,14 +3,14 @@ import pandas as pd
 from sklearn.preprocessing import MinMaxScaler, LabelEncoder
 from data.raw_csi import import_clean_data
 from data.utils import DatasetObject
-from data import process_data
+from data.transformation import *
 from torch import Tensor
 from torch.utils.data import TensorDataset, DataLoader
 from torch.utils.data import DataLoader, random_split
 from logger import logging
 from torchvision.datasets.folder import ImageFolder
-
-
+from data.spectrogram import import_data
+from data.transformation import label_encode
 
 def create_dataloaders(X_train, y_train, X_test, y_test, train_batch_sizes=64, test_batch_sizes=200):
     traindataset = TensorDataset(Tensor(X_train),Tensor(y_train).long())
@@ -18,3 +18,10 @@ def create_dataloaders(X_train, y_train, X_test, y_test, train_batch_sizes=64, t
     train_loader = DataLoader(traindataset, batch_size=train_batch_sizes, shuffle=True, num_workers=1, drop_last=True)
     test_loader = DataLoader(testdataset, batch_size=test_batch_sizes, shuffle=True, num_workers=1)
     return train_loader, test_loader
+
+def prepare_data(dirc):
+    X,y  = import_data(dirc)
+    X = X.reshape(*X.shape,1).transpose(0,3,1,2)
+    y,lb = label_encode(y)
+    # print(f'shape: X:{X.shape} y:{y.shape}',"\tclass: ",lb.classes_)
+    return X,y,lb
