c848aa6e774a8e6e5628cb5c92eaf75811158155
hslau44 <hslau44@gmail.com>
2021-04-23 21:50:21 +0100

    add NT_Xent_ to replace NT_Xent for adding support of multiview

--- losses/__init__.py
+++ losses/__init__.py
@@ -76,6 +76,73 @@ class Contrastive_multiview_loss(nn.Module):
         return loss
 
 
+
+class NT_Xent_(nn.Module):
+    """
+    NT_Xent with additional support for num_repre (number of representation) > 2
+
+    Attributes:
+    batch_size (int): batch size
+    num_repre (int): number of representation
+    temperature (float): smoothness
+
+    """
+
+    def __init__(self,batch_size,num_repre=2,temperature=0.1):
+        super(NT_Xent_, self).__init__()
+        if num_repre < 2:
+            raise ValueError('num_repre must be >=2')
+        self.batch_size = batch_size
+        self.num_repre = num_repre
+        self.mtx_size = batch_size*num_repre
+        self.positive_mask = self.generate_mask(1)
+        self.negative_mask = self.generate_mask(0)
+        self.temperature = temperature
+        self.xent = nn.CrossEntropyLoss(reduction="sum")
+        self.cossim = nn.CosineSimilarity(dim=2)
+
+
+    def forward(self,*tensors):
+        """
+        Argument
+        tensors (torch.Tensor):
+        """
+        assert tensors[0].shape[0] == self.batch_size,f'batch size not matching, expecting size: {self.batch_size}'
+        assert len(tensors) == self.num_repre,f'number of representation not matching, expecting size: {self.num_repre}'
+        z = torch.cat(tensors, dim=0)
+        s = self.cossim(z.unsqueeze(1),z.unsqueeze(0))/self.temperature
+        positive_samples = s[self.positive_mask]
+        positive_samples = positive_samples.reshape(self.mtx_size,-1)
+        positive_samples = torch.cat(torch.split(positive_samples,1,1))
+        print(f'positive shape {positive_samples.shape}')
+        negative_samples = s[self.negative_mask]
+        negative_samples = negative_samples.reshape(self.mtx_size,-1)
+        negative_samples = negative_samples.repeat(num_repe-1,1)
+        print(f'negative shape {negative_samples.shape}')
+        logits = torch.cat((positive_samples, negative_samples), dim=1)
+        labels = torch.zeros(logits.shape[0]).long().to(logits.device)
+        print(f'logit shape {logits.shape}')
+        loss = self.xent(logits, labels)
+        loss /= self.mtx_size
+        return loss
+
+    def generate_mask(self,mask_type=0):
+        if mask_type != 1 and mask_type != 0:
+            raise ValueError('Mask_type must be equal {0,1}')
+        if mask_type:
+            mask = torch.zeros((self.mtx_size, self.mtx_size), dtype=bool)
+        else:
+            mask = torch.ones((self.mtx_size, self.mtx_size), dtype=bool)
+            mask = mask.fill_diagonal_(0)
+        for i in range(self.mtx_size):
+            for j in range(i,self.mtx_size,self.batch_size):
+                if i != j:
+                    mask[i,j] = mask_type
+                    mask[j,i] = mask_type
+        return mask
+
+
+
 class SupConLoss(nn.Module):
     """
 
