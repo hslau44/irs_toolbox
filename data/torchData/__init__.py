9283d18f2ed6d3bd3c560908504e27780a9bc91a
hslau44 <hslau44@gmail.com>
2021-08-30 15:38:02 +0100

    add new class DataSelection for substituting dataSelection()

--- data/torchData/__init__.py
+++ data/torchData/__init__.py
@@ -99,6 +99,79 @@ def dataSelection(df,split='loov',nuc='NUC1',room=1,spc=None):
     return train,val,test
 
 
+class DataSelection(object):
+
+    def __init__(self,split,train_sub,val_sub=None,nuc=None,room=None,sample_per_class=None):
+        """
+
+        Argument:
+        split  (str): spliting by either
+        train_sub (str/float):
+        val_sub   (str/float):
+        nuc (str):
+        room (str):
+        sample_per_class (int/bool): ,default {None}
+        """
+        self.nuc = nuc
+        self.room = room
+        self.split = split
+        # argument for 'random' split
+        if self.split == 'random':
+            assert val_sub < train_sub
+            self.train_sub = train_sub
+            self.atrain_sub = 1-val_sub/train_sub
+        # argument for 'loov' split
+        elif split == 'loov':
+            self.train_sub = train_sub
+            self.val_sub = val_sub
+        # sub_sampling
+        self.spc = sample_per_class
+        # default argument
+        if nuc == None:
+            self.nuc = ['NUC1','NUC2']
+        if room == None:
+            self.room = [1,2]
+        self.num_class = 0
+        self.encoder = LabelEncoder()
+
+    def __call__(self,df):
+
+        df['activity'] = self.encoder.fit_transform(df['activity'])
+
+        if isinstance(nuc,list):
+            df = df[df['nuc'].isin(self.nuc)]
+        else:
+            df = df[df['nuc'] == self.nuc]
+
+        if isinstance(room,list):
+            df = df[df['room'].isin(self.room)]
+        else:
+            df = df[df['room'] == self.room]
+
+        self.num_class = df['activity'].nunique()
+
+        train,val,test = None,None,None
+
+        if self.split == 'random':
+            train,test = random_split(df,train_size=self.train_sub)
+            if self.val_sub:
+                train,val = random_split(train,train_size=self.atrain_sub)
+        elif self.split == 'loov':
+            if self.val_sub:
+                train,val,test = leaveOneOut_split(df,testsub=self.train_sub,valsub=self.val_sub)
+            else:
+                train,test = leaveOneOut_split(df,testsub=self.train_sub,valsub=None)
+
+        if isinstance(self.spc,int):
+            train = resampling(train,'activity',oversampling=False)
+            train,_ = random_split(train,train_size=self.num_class*self.spc)
+        test = resampling(test,'activity',oversampling=False)
+
+        if self.val_sub:
+            return train,val,test
+        else:
+            return train,test
+
 def dataLoader_CnnLstmS(df):
     """dataLoader_cnnLstmS"""
     transform = T.Compose([ReduceRes(),Unsqueeze(),ToStackImg(25)])
