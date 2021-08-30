46598d812120e7b2a4ed3cf1146bbc5a98a6605b
hslau44 <hslau44@gmail.com>
2021-08-30 13:45:55 +0100

    add change name to data_selection, add data_loader

--- data/torchData/__init__.py
+++ data/torchData/__init__.py
@@ -1,15 +1,24 @@
+from sklearn.preprocessing import LabelEncoder
+import torchvision.transforms as T
+from torch.utils.data import DataLoader
+from data.torchData.custom_data import filepath_dataframe
+from data.torchData.utils import DatasetObject
 from data.torchData.transformation import *
 
-def pipeline1(df):
+def dataSelection_Set1(df):
     """To test the model under lab environemnt by leave-one-person-out validation"""
+    lb = LabelEncoder()
+    df['activity'] = lb.fit_transform(df['activity'])
     df = df[df['nuc'] == 'NUC1']
     df = df[df['room'] == 1]
-    train,val,test = leaveOnePersonOut_split(df,testsub='One',valsub='Four')
+    train,val,test = leaveOneOut_split(df,testsub='One',valsub='Four')
     test = resampling(test,'activity',oversampling=False)
     return train,val,test
 
-def pipeline1b(df):
+def dataSelection_Set2(df):
     """To test the model under lab environemnt by random-split validation"""
+    lb = LabelEncoder()
+    df['activity'] = lb.fit_transform(df['activity'])
     df = df[df['nuc'] == 'NUC1']
     df = df[df['room'] == 1]
     train,test = random_split(df,train_size=0.8)
@@ -17,46 +26,52 @@ def pipeline1b(df):
     test = resampling(test,'activity',oversampling=False)
     return train,val,test
 
-def pipeline2(df):
+def dataSelection_Set3(df):
     """To test the model under unseen environemnt"""
+    lb = LabelEncoder()
+    df['activity'] = lb.fit_transform(df['activity'])
     df = df[df['nuc'] == 'NUC1']
     train = df[df['room'] == 1]
     test  = df[df['room'] == 2]
 
-    train,val, _ = leaveOnePersonOut_split(train,testsub='One',valsub='Four')
-    _ , _ , test = leaveOnePersonOut_split(test,testsub='One',valsub='Four')
+    train,val, _ = leaveOneOut_split(train,testsub='One',valsub='Four')
+    _ , _ , test = leaveOneOut_split(test,testsub='One',valsub='Four')
 
     test = resampling(test,'activity',oversampling=False)
     return train,val,test
 
 
-def pipeline3(df):
+def dataSelection_Set4(df):
     """ To test if combine data provide better generalizability"""
     nuc = ['NUC1','NUC2']
     rooms = [1,2]
 
+    lb = LabelEncoder()
+    df['activity'] = lb.fit_transform(df['activity'])
     df = df[df['nuc'].isin(nuc)]
     df = df[df['room'].isin(rooms)]
 
-    train,val,test = leaveOnePersonOut_split(df,testsub='One',valsub='Four')
+    train,val,test = leaveOneOut_split(df,testsub='One',valsub='Four')
     test = resampling(test,'activity',oversampling=False)
     return train,val,test
 
 
-def pipeline4(df,spc=5):
+def dataSelection_Set5(df,spc=5):
     """To test model performance under low number of sample"""
+    lb = LabelEncoder()
+    df['activity'] = lb.fit_transform(df['activity'])
     df = df[df['nuc'] == 'NUC1']
     df = df[df['room'] == 1]
     num_class = df['activity'].nunique()
 
-    train,val,test = leaveOnePersonOut_split(df,testsub='One',valsub='Four')
+    train,val,test = leaveOneOut_split(df,testsub='One',valsub='Four')
 
     train = resampling(train,'activity',oversampling=False)
     train,_ = random_split(train,train_size=spc*num_class)
     test = resampling(test,'activity',oversampling=False)
     return train,val,test
 
-def generalPipeline(df,split='loov',nuc='NUC1',room=1,spc=None):
+def dataSelection(df,split='loov',nuc='NUC1',room=1,spc=None):
 
     if isinstance(nuc,list):
         df = df[df['nuc'].isin(nuc)]
@@ -74,10 +89,29 @@ def generalPipeline(df,split='loov',nuc='NUC1',room=1,spc=None):
         train,test = random_split(df,train_size=0.8)
         train,val = random_split(train,train_size=0.875)
     elif split == 'loov':
-        train,val,test = leaveOnePersonOut_split(df,testsub='One',valsub='Four')
+        train,val,test = leaveOneOut_split(df,testsub='One',valsub='Four')
 
     if isinstance(spc,int):
         train = resampling(train,'activity',oversampling=False)
         train,_ = random_split(train,train_size=num_spc*spc)
     test = resampling(test,'activity',oversampling=False)
     return train,val,test
+
+
+def dataLoader_CnnLstmS(df):
+    """dataLoader_cnnLstmS"""
+    transform = T.Compose([ReduceRes(),Unsqueeze(),ToStackImg(25)])
+    datasetobj = DatasetObject(filepaths=df['fullpath'].to_numpy(),
+                               label=df['activity'].to_numpy(),
+                               transform=transform)
+    dataloader = DataLoader(datasetobj, batch_size=64, shuffle=False, num_workers=0, pin_memory=True)
+    return dataloader
+
+def dataLoader_CnnS(df):
+    """dataLoader_CnnS"""
+    transform = T.Compose([ReduceRes(),Unsqueeze()])
+    datasetobj = DatasetObject(filepaths=df['fullpath'].to_numpy(),
+                               label=df['activity'].to_numpy(),
+                               transform=transform)
+    dataloader = DataLoader(datasetobj, batch_size=64, shuffle=False, num_workers=0, pin_memory=True)
+    return dataloader
