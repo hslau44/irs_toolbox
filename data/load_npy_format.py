0ad0282f70bb8c8d872ae96fd5b57964ebb060f1
hslau44 <hslau44@gmail.com>
2021-09-03 20:04:20 +0000

    code successfully run on cloud

--- data/load_npy_format.py
+++ data/load_npy_format.py
@@ -1,4 +1,3 @@
-
 import numpy as np
 import pandas as pd
 import time
@@ -12,7 +11,7 @@ def assure_path_exists(directory,makedir=False):
             os.makedirs(directory)
             return True
         else:
-            raise FileNotFoundError(f'{directory} does not exist')
+            raise FileNotFoundError()
     return True
 
 def load_npy(df,save_dir='.',splitchar='\\',verbose=True):
@@ -33,11 +32,16 @@ def load_npy(df,save_dir='.',splitchar='\\',verbose=True):
         arr = pd.read_csv(df.loc[idx,'fullpath'],header=None).to_numpy()
         new_filepath = df.loc[idx,'new_filepath']
         np.save(new_filepath,arr)
-        if (idx+1)%(len(df)/10) == 0: print('>',end='')
+        if verbose:
+            if (idx+1)%(len(df)//10) == 0: 
+                print('>', end='')
     return
 
+
 if __name__ == '__main__':
-    directory = 'E:/external_data/opera_csi/Session_2/experiment_data/experiment_data/exp_7_amp_spec_only/npy_format'
-    df = filepath_dataframe(directory)
-    savepath = 'E:\\external_data\\opera_csi\\Session_2\\experiment_data\\experiment_data\\exp_7_amp_spec_only\\npy_format'
-    load_npy(df,save_dir=savepath)
+    directory = '/root/irs_toolbox/data/exp_7_amp_spec_only/spectrogram_multi'
+    splitchar='/'
+    
+    df = filepath_dataframe(directory,splitchar)
+    savepath = '/root/irs_toolbox/data/exp_7_amp_spec_only/npy_format'
+    load_npy(df,save_dir=savepath,splitchar=splitchar)
