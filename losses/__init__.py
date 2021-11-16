45cd00fe6037c72e0ab8eccfa8efca8cd82e7996
hslau44 <hslau44@gmail.com>
2021-11-16 13:00:05 +0000

    update JSD

--- losses/__init__.py
+++ losses/__init__.py
@@ -180,3 +180,19 @@ class SupConLoss(nn.Module):
         loss = loss.view(anchor_count, batch_size).mean()
 
         return loss
+
+class JSD(nn.Module):
+
+    def __init__(self):
+        super(JSD, self).__init__()
+
+    def forward(self, net_1_logits, net_2_logits):
+        net_1_probs =  F.softmax(net_1_logits, dim=1)
+        net_2_probs=  F.softmax(net_2_logits, dim=1)
+
+        m = 0.5 * (net_1_probs + net_1_probs)
+        loss = 0.0
+        loss += F.kl_div(F.log_softmax(net_1_logits, dim=1), total_m, reduction="batchmean")
+        loss += F.kl_div(F.log_softmax(net_2_logits, dim=1), total_m, reduction="batchmean")
+
+        return (0.5 * loss)
